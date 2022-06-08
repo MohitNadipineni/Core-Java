6599fe820b1506a4b9e506da08825caa0f7497f8
MohitNadipineni <84355183+MohitNadipineni@users.noreply.github.com>
2022-06-08 11:05:12 -0400

    Add files via upload

---
+++ Lesson13.java
@@ -0,0 +1,20 @@
+class Lesson13{
+	public static void main (String[]  args) {
+		int month_Days[];
+		month_Days = new int[12];
+		month_Days[0] = 31;
+		month_Days[1] = 28;
+		month_Days[2] = 31;
+		month_Days[3] = 30;
+		month_Days[4] = 31;
+		month_Days[5] = 30;
+		month_Days[6] = 31;
+		month_Days[7] = 31;
+		month_Days[8] = 30;
+		month_Days[9] = 31;
+		month_Days[10] = 30;
+		month_Days[11] = 31;
+		
+		System.out.println("April has"+ month_Days[3]+"days");
+	}
+}
---
+++ Lesson14.java
@@ -0,0 +1,11 @@
+class Lesson14{
+	public static void main (String[] args) {
+		String x = args[0];
+		String y = args[1];
+		
+		double a = Double.parseDouble(args[0]);
+		double b = Double.parseDouble(args[1]);
+		
+		System.out.println(a+b);
+	}
+}
