41735fcc8373b919df072ce3f3672ab264ce65f5
MohitNadipineni <84355183+MohitNadipineni@users.noreply.github.com>
2022-06-13 12:57:56 -0400

    Add files via upload
    Encapsulation concept using get and set methods.

---
+++ Student.java
@@ -0,0 +1,26 @@
+
+public class Student {
+	 private String studentname;
+	 private int rollno;
+	 private char grade;
+	 
+	 public void setStudentname(String newStudentname) {
+		 studentname = newStudentname;
+	 }
+	 public String getStudentname() {
+		 return studentname;
+	 }
+	 
+	 public void setRollno(int newRollno) {
+		 rollno = newRollno;
+	 }
+	 public int getRollno() {
+		 return rollno;
+	 }
+	 public void setGrade(char newGrade) {
+		 grade = newGrade;
+	 }
+	 public char getGrade() {
+		 return grade;
+	 }
+}
